17add8b7caba50df61ba9b71d124b540035bf70e
marianog <marianogonzalezazcon@gmail.com>
2023-03-21 21:31:59 -0300

    avance en ejercicio tp_1_2

--- OO2_workspace/tp1_2_Biblioteca/src/test/java/ar/edu/unlp/info/oo2/biblioteca/BibliotecaTest.java
+++ OO2_workspace/tp1_2_Biblioteca/src/test/java/ar/edu/unlp/info/oo2/biblioteca/BibliotecaTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 
 
 public class BibliotecaTest {
-	Biblioteca biblioteca;
+	Biblioteca biblioteca, bibliotecaVacia;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		biblioteca = new Biblioteca();
+		bibliotecaVacia = new Biblioteca();
+		biblioteca.setExporter(new JSONadapter());
 		biblioteca.agregarSocio(new Socio("Arya Stark", "needle@stark.com", "5234-5"));
 		biblioteca.agregarSocio(new Socio("Tyron Lannister", "tyron@thelannisters.com",
 		"2345-2"));
@@ -21,6 +23,7 @@ public class BibliotecaTest {
     @Test
     public void testNombreCompleto() {
         System.out.println(biblioteca.exportarSocios());
+        System.out.println(bibliotecaVacia.exportarSocios());
     }
 
 }
