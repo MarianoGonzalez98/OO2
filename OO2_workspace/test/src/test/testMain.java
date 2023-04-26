5697cd7a1417da3c0c98d2b0e27529ca30d9e05a
marianog <marianogonzalezazcon@gmail.com>
2023-04-25 23:56:53 -0300

    commit normal

---
+++ OO2_workspace/test/src/test/testMain.java
@@ -0,0 +1,19 @@
+package test;
+public class testMain {
+	double x;
+	static double varGlobal1; //se inicializa automáticamente en 0.0
+	static StringBuilder varGlobal2;	//se inicializa automáticamente en null
+	static int variable = 1; //se inicializa automáticamente en 0
+	public static void main(String[] args) {
+		varGlobal2 = new StringBuilder("asd"); //la variable ahora contiene la direccion de memoria de la heap
+		double varLocal1; //no se inicializa con ningún valor
+		int j = 3;
+		double variable = 4.0;
+		varLocal1 = 2;
+		varGlobal1 = 1.0;
+		System.out.println("variables: "+ varGlobal1 +" "+ varGlobal2 +" "+variable +" "+j);
+	}
+	
+}
+
+//
